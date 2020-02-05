80fb2cdae22989b5aeced3b48b4150687defbf23
Carlos Encinas <carlos-encinas@hotmail.com>
2020-02-05 11:45:50 -0700

    a

--- Assets/Scripts/MovPlayer.cs
+++ Assets/Scripts/MovPlayer.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Axis : MonoBehaviour
+public class MovPlayer : MonoBehaviour
 {
-    
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +13,6 @@ public class Axis : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
-    float h = Input.GetAxis("Horizontal");
-    float v = Input.GetAxis("Vertical");
-    float hPos = h * hRange;
-    float vPos = v * vRange;
-
-
+        
     }
 }
